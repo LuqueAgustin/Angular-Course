731b25a637d3fe7af315eccb0c46c00d16d45506
LuqueAgustin <agustin.l.luque@accenture.com>
2022-09-13 13:04:06 -0300

    Section-7 Highlight Fix

--- section-7/src/app/better-highlight/better-highlight.directive.ts
+++ section-7/src/app/better-highlight/better-highlight.directive.ts
@@ -12,8 +12,8 @@ import {
 })
 export class BetterHighlightDirective implements OnInit {
   @Input() defaultColor: string = 'transparent';
-  @Input() highlightColor: string = 'blue';
-  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
+  @Input('appBetterHighlight') highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
 
   }
@@ -24,7 +24,7 @@ export class BetterHighlightDirective implements OnInit {
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
-    this.backgroundColor = this.backgroundColor;
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
