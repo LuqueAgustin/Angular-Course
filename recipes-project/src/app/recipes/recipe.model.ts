0e0769585139b07713ff1d0a554a9507ecfea60b
LuqueAgustin <agustin.l.luque@accenture.com>
2022-09-14 15:34:41 -0300

    Section-10 Completed

--- recipes-project/src/app/recipes/recipe.model.ts
+++ recipes-project/src/app/recipes/recipe.model.ts
@@ -1,7 +1,13 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
+    public ingredients: Ingredient[];
     constructor(
         public name: string,
         public description: string,
-        public imagePath: string
-        ) {}
+        public imagePath: string,
+        ingredients: Ingredient[],
+        ) {
+            this.ingredients = ingredients;
+        }
 }
--- recipes-project/src/app/recipes/recipe.service.ts
+++ recipes-project/src/app/recipes/recipe.service.ts
@@ -1,16 +1,54 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is simple a test', 'https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_1280.jpg'),
-        new Recipe('Another test recipe', 'This is simple a test', 'https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_1280.jpg')
-      ];
+        new Recipe(
+            'Combo Braker Burger', 
+            'Simple but Letal!', 
+            'https://cdn.pixabay.com/photo/2019/01/29/18/05/burger-3962997_1280.jpg',
+            [
+                new Ingredient('Buns', 1),
+                new Ingredient('Pickles', 1),
+                new Ingredient('Cheese', 2),
+                new Ingredient('Red Onion', 1),
+                new Ingredient('Bacon', 4),
+                new Ingredient('Tomato', 1),
+                new Ingredient('Meat', 2),
+                new Ingredient('Mustard', 1),
+                new Ingredient('Lettuce', 1),
+                new Ingredient('French Fries', 20),
+                
+            ]),
+        new Recipe(
+            'Pork Schnitzel with Lemon', 
+            'Even cooler with some fries on the side!', 
+            'https://cdn.pixabay.com/photo/2018/03/31/19/31/schnitzel-3279055_1280.jpg',
+            [
+                new Ingredient('Pork Meat', 1),
+                new Ingredient('French Fries', 20),
+                new Ingredient('Lemon', 2),
+                new Ingredient('Bread Crumbs', 1),
+                new Ingredient('Eggs', 2),
+            ]
+        )
+    ];
+
+    constructor(private shoppingListService: ShoppingListService) {
+
+    }
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
     
 }
